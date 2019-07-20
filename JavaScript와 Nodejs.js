7bbb9e94bdc54b22eaac03329d79663ac598fd6b
wh2per <93negaro@gmail.com>
2019-07-20 22:46:01 +0900

    JavaScript와 Node.js 완료

--- JavaScript와 Nodejs.js
+++ JavaScript와 Nodejs.js
@@ -31,7 +31,7 @@ console.log(letter);
 
 asdfasdf
 
-3. 쿼리스트링
+3. 쿼리스트링 (get 방식)
 http://127.0.0.1:3000/?id=HTML에서 ?뒤의 문자열을 쿼리스트링이라고 한다
 ex)
 var http = require('http');
@@ -243,10 +243,12 @@ slowfunc(a);
 13. PM2(패키지 매니저) 사용법
 서버 시작 : pm2 start main.js
 모니터 : pm2 monit
+에러 로그 : pm2 log
 서버 목록 : pm2 list
 서버 중지 : pm2 stop main.js
 서버 재시작 : pm2 restart main.js
 서버 삭제 : pm2 delete main.js
+* 소스코드 수정 시 자동 재시작 : pm2 start main.js --watch
 
 14. POST 방식으로 전송된 데이터를 받아서 파일로 저장하는 방법
 * request.on('data',function(data){});
@@ -269,3 +271,99 @@ request.on('end',function(data){
 >> { title : 'A', description : 'B'}
 
 15. 전송된 POST 데이터를 받아서 파일에 저장
+POST 데이터를 title과 description 변수에 저장한 후 fs.writeFile() 실행
+첫번째 인자는 저장할 경로(파일이름), 두번째 인자는 내용
+ex)
+fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+  response.writeHead(200);
+  response.end('success');
+});
+
+16. 페이지 리다이렉션
+response.writeHead()의 첫번째 인자를 302 // 다른 페이지로 보낸다는 신호
+두번째 인자에는 보낼 페이지의 주소
+ex)
+response.writeHead(302, {Location: `/?id=${title}`});
+
+17. 기존 파일 이름 및 내용 수정
+기존 파일 이름(id)를 새로운 이름(title)로 수정할 때는 fs.rename() 사용
+파일의 내용은 fs.writeFile()로 수정
+ex)
+fs.rename(`data/${id}`,`data/${title}`,function(error){
+  fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+    response.writeHead(302, {Location: `/?id=${title}`});
+    response.end();
+  });
+
+18. 글 삭제 버튼
+delete 기능은 get 방식으로 구현하면 안됨!
+form 태그를 이용하여 post 방식으로 구현하자
+ex)
+<form action="delete_process" method="post">
+  <input type="hidden" name = "id" value = "${title}">
+  <input type="submit" value="delete">
+</form>>
+
+19. 파일 삭제
+fs.unlink()에 파일 경로와 콜백함수를 넣어서 실행
+ex)
+fs.unlink(`data/${id}`,function(error){
+  response.writeHead(302, {Location: `/`});
+  response.end();
+});
+
+20. 객체 모듈화
+객체를 모듈화 하여 다른 파일에서 사용할 수 있다
+ex)
+* mpart.js
+var M = {
+  v:'v',
+  f:function(){
+    console.log(this.v);
+  }
+}
+module.exports = M;   // M이라는 객체를 이 모듈 바깥에서 사용할 수 있도록 export한다는 뜻
+
+* muse.js
+var part = require('./mpart.js');
+part.f();
+
+21. 입력정보 보안 방법
+readFile()에 첫 번째 인자를 var filteredId = path.parse(queryData.id).base; 로 대체
+path.parse().base 를 이용해 쿼리스트링을 숨기자!
+ex)
+var path = requeir('path');
+var filteredId = path.parse(queryData.id).base;
+fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+  // 파일 읽기
+});
+
+22. 출력정보 보안 방법
+sanitizeHtml() 을 이용하여 출력 시 스크립트 부분을 제거한다
+게시판에 강제로 스크립트를 적어서 submit해도 걸러진다
+HTML 태그는 거르고 내용은 살려준다
+ex)
+fs.readdir('./data', function(error, filelist){
+  var filteredId = path.parse(queryData.id).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+    var title = queryData.id;
+    var sanitizedTitle = sanitizeHtml(title);
+    var sanitizedDescription = sanitizeHtml(description, {allowedTags:['h1']});
+    var list = template.List(filelist);
+    var HTML = template.HTML(sanitizedTitle, list,
+      `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+      `<a href = "/create">create</a>
+      <a href = "/update?id=${sanitizedTitle}">update</a>
+      <form action="delete_process" method="post">
+        <input type="hidden" name = "id" value = "${sanitizedTitle}">
+        <input type="submit" value="delete">
+      </form>`
+    );
+    response.writeHead(200);
+    response.end(HTML);
+  });
+});
+
+* 허용할 태그는 객체로 만들어 sanitizeHtml() 함수의 두 번째 인자로 넘겨준다
+ex)
+var sanitizedDescription = sanitizeHtml(description, {allowedTags:['h1']});
